cda1fbb3259ca0306ba5c37eaeebd2fc279f7333
Dagmawi2000 <133396227+Dagmawi2000@users.noreply.github.com>
2025-06-06 18:16:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
+        "undici": false,
       };
     }
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
