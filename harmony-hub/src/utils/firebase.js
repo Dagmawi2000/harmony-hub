7360d8a29fece8cad76da01f138570356202d141
Dagmawi2000 <133396227+Dagmawi2000@users.noreply.github.com>
2025-06-06 16:28:25 +0200

    Update firebase.js

--- harmony-hub/src/utils/firebase.js
+++ harmony-hub/src/utils/firebase.js
@@ -1,19 +1,28 @@
+'use client';
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfqVj__UhhUj4DvVZlirMY97grWBghVaQ",
-  authDomain: "harmony-hub-web.firebaseapp.com",
-  projectId: "harmony-hub-web",
-  storageBucket: "harmony-hub-web.firebasestorage.app",
-  messagingSenderId: "552286400488",
-  appId: "1:552286400488:web:3cfab69508f58c43cd3e5d",
-  measurementId: "G-ZNNJMYDDPT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
+let app;
+try {
+  app = initializeApp(firebaseConfig);
+} catch (error) {
+  if (!/already exists/.test(error.message)) {
+    console.error('Firebase initialization error', error.stack);
+  }
+}
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
 export default app;
